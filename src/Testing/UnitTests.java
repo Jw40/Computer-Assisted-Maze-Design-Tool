ac81bd49c49c950435defe6975b0188794eb1597
alter3401 <sampappalardo@hotmail.com>
2022-06-05 16:59:07 +1000

    Add testunit class

---
+++ src/Testing/UnitTests.java
@@ -0,0 +1,14 @@
+package Testing;
+
+import Controller.*;
+
+public class UnitTests {
+    // Checks if the maze object has been created.
+        public Maze MazeCreated(Maze maze)
+        {
+
+            return maze;
+        }
+
+
+}
