a06c35cd3387e4891cc6cb3e9caf5df071b6ddd8
Salvatore Pappalardo <90760564+alter3401@users.noreply.github.com>
2022-06-08 18:43:08 +1000

    Need Assistance

--- src/Testing/UnitTests.java
+++ src/Testing/UnitTests.java
@@ -9,6 +9,18 @@ import java.awt.*;
 public class UnitTests {
     // Checks if the maze object has been saved.
 
+    public GUI gui;
+
+    {
+        try {
+            gui = new GUI();
+        } catch (AWTException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public Maze maze = new Maze();
+
+  
     Maze maze1 = new Maze();
     Maze maze2 = new Maze(14,6);
     Maze maze3 = new Maze(50,50);
@@ -16,7 +28,7 @@ public class UnitTests {
     Maze maze5 = new Maze(500,500);
     Maze maze6 = new Maze(0,0);
 
-    
+
 
 
 
